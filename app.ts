46b9a1295a2c33bb742cbbb4817da6107a021fa7
Lior Grishin <94124882+lorGR@users.noreply.github.com>
2022-02-26 19:04:16 +0200

    JS Tip Calculator Changes
    - Finished Logic
    - Presented on DOM
    - Added Style

--- app.ts
+++ app.ts
@@ -1,5 +1,76 @@
 const userBill:HTMLInputElement = document.querySelector('#billAmount')
-console.log(userBill.valueAsNumber)
 
 const userTip:HTMLSelectElement = document.querySelector('#serviceRate')
-console.log(userTip.selectedIndex)
+
+const userShareBill:HTMLInputElement = document.querySelector('#shareBill')
+
+const tipAmountTitle:HTMLHeadingElement = document.querySelector('#tipTitle')
+const tipAmount:HTMLParagraphElement = document.querySelector('#tipAmount')
+const tipEach:HTMLParagraphElement = document.querySelector('#tipEach')
+
+const form:HTMLFormElement = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const userInfo:CardInfo = new CardInfo()
+    const tip = calculateTip(userInfo)
+    tipAmountTitle.classList.remove('hidden')
+    tipAmount.classList.remove('hidden')
+    console.log(tip.toFixed(2))
+    tipAmount.textContent = (`${tip.toFixed(2)}₪`)
+})
+
+class CardInfo {
+    userBill: number
+    userTip: number
+    userShareBill: number
+    constructor(){
+        this.userBill = userBill.valueAsNumber
+        this.userTip = userTip.selectedIndex
+        this.userShareBill = userShareBill.valueAsNumber
+    }
+}
+
+function calculateTip(cardInfo:CardInfo):number {
+    validSharedBill(cardInfo)
+    checkUserTip(cardInfo)
+    const tip:number = (cardInfo.userBill * cardInfo.userTip) / cardInfo.userShareBill
+    return tip
+}
+
+function checkUserTip(cardInfo:CardInfo):number{
+    if(cardInfo.userTip === 0){
+        return cardInfo.userTip = .05
+    }else if(cardInfo.userTip === 1){
+        return cardInfo.userTip = .1
+    }
+    else if(cardInfo.userTip === 2){
+        return cardInfo.userTip = .15
+    }
+    else if(cardInfo.userTip === 3){
+        return cardInfo.userTip = .2
+    }
+}
+
+function validSharedBill(cardInfo:CardInfo):number{
+    if(cardInfo.userShareBill === 0 || isNaN(cardInfo.userShareBill)){
+        removeShownAddHidden()
+        return cardInfo.userShareBill = 1
+    }
+    else if(cardInfo.userShareBill === 1){
+        removeShownAddHidden()
+    }
+    else{
+        removeHiddenAddShown()
+    }
+}
+
+function removeHiddenAddShown(){
+    tipEach.classList.remove('hidden')
+    tipEach.classList.add('shown')
+}
+
+function removeShownAddHidden(){
+    tipEach.classList.remove('shown')
+    tipEach.classList.add('hidden')
+}
+
--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,64 @@
 var userBill = document.querySelector('#billAmount');
-console.log(userBill.valueAsNumber);
 var userTip = document.querySelector('#serviceRate');
-console.log(userTip.selectedIndex);
+var userShareBill = document.querySelector('#shareBill');
+var tipAmountTitle = document.querySelector('#tipTitle');
+var tipAmount = document.querySelector('#tipAmount');
+var tipEach = document.querySelector('#tipEach');
+var form = document.querySelector('form');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var userInfo = new CardInfo();
+    var tip = calculateTip(userInfo);
+    tipAmountTitle.classList.remove('hidden');
+    tipAmount.classList.remove('hidden');
+    console.log(tip.toFixed(2));
+    tipAmount.textContent = (tip.toFixed(2) + "\u20AA");
+});
+var CardInfo = /** @class */ (function () {
+    function CardInfo() {
+        this.userBill = userBill.valueAsNumber;
+        this.userTip = userTip.selectedIndex;
+        this.userShareBill = userShareBill.valueAsNumber;
+    }
+    return CardInfo;
+}());
+function calculateTip(cardInfo) {
+    validSharedBill(cardInfo);
+    checkUserTip(cardInfo);
+    var tip = (cardInfo.userBill * cardInfo.userTip) / cardInfo.userShareBill;
+    return tip;
+}
+function checkUserTip(cardInfo) {
+    if (cardInfo.userTip === 0) {
+        return cardInfo.userTip = .05;
+    }
+    else if (cardInfo.userTip === 1) {
+        return cardInfo.userTip = .1;
+    }
+    else if (cardInfo.userTip === 2) {
+        return cardInfo.userTip = .15;
+    }
+    else if (cardInfo.userTip === 3) {
+        return cardInfo.userTip = .2;
+    }
+}
+function validSharedBill(cardInfo) {
+    if (cardInfo.userShareBill === 0 || isNaN(cardInfo.userShareBill)) {
+        removeShownAddHidden();
+        return cardInfo.userShareBill = 1;
+    }
+    else if (cardInfo.userShareBill === 1) {
+        removeShownAddHidden();
+    }
+    else {
+        removeHiddenAddShown();
+    }
+}
+function removeHiddenAddShown() {
+    tipEach.classList.remove('hidden');
+    tipEach.classList.add('shown');
+}
+function removeShownAddHidden() {
+    tipEach.classList.remove('shown');
+    tipEach.classList.add('hidden');
+}
